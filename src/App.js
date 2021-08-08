7a2b8c63019b06bbc7df5bf49f116f5f0b12bc19
yugmani <yugmani@yahoo.com>
2021-08-08 16:50:39 -0700

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  // let name = 'Yooog';""
+  const [name, setName] = useState('yg');
+
+  // let hobby = 'playing soccer';
+
+  // let location = 'Los Angeles, CA';
+  const [country, setCountry] = useState('CA');
+  const [hobby, setHobby] = useState('playing soccer');
+
+  const mystyle = {
+    color: 'white',
+    fontSize: '60px'
+  };
+
+  const h3style = {
+    fontSize: '45px',
+    color: 'yellow'
+  };
+
+  const hobbystyle = {
+    color: 'gray',
+    fontSize: '25px'
+  };
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="profile">
+      <h1 style={mystyle}>Hi, I am {name}!</h1>
+      <h3 style={h3style}>I am from {country}.</h3>
+      <p style={hobbystyle}>I love {hobby}!</p>
+      <button onClick={() => setName('yoog')}>Change Name</button>
+      <button onClick={() => setCountry('USA')}>Change Country</button>
+      <button onClick={() => setHobby('Coding')}>Change Hobby</button>
     </div>
   );
 }
